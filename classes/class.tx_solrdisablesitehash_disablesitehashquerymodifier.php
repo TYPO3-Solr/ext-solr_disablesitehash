82ae21061e381be86d6c8a0dcb35eeb58012e5db
Olivier Dobberkau <olivier.dobberkau@dkd.de>
2015-04-17 13:50:57 +0200

    added files and folders
    this is the initial commit of the folders and files

---
+++ classes/class.tx_solrdisablesitehash_disablesitehashquerymodifier.php
@@ -0,0 +1,58 @@
+<?php
+/***************************************************************
+*  Copyright notice
+*
+*  (c) 2010 Ingo Renner <ingo@typo3.org>
+*  All rights reserved
+*
+*  This script is part of the TYPO3 project. The TYPO3 project is
+*  free software; you can redistribute it and/or modify
+*  it under the terms of the GNU General Public License as published by
+*  the Free Software Foundation; either version 2 of the License, or
+*  (at your option) any later version.
+*
+*  The GNU General Public License can be found at
+*  http://www.gnu.org/copyleft/gpl.html.
+*
+*  This script is distributed in the hope that it will be useful,
+*  but WITHOUT ANY WARRANTY; without even the implied warranty of
+*  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+*  GNU General Public License for more details.
+*
+*  This copyright notice MUST APPEAR in all copies of the script!
+***************************************************************/
+
+
+require_once($GLOBALS['PATH_solr'] . 'interfaces/interface.tx_solr_querymodifier.php');
+
+/**
+ * Disables the use of the sitehash filter in Solr queries
+ *
+ * @author	Ingo Renner <ingo@typo3.org>
+ * @package TYPO3
+ * @subpackage solr_disablesitehash
+ */
+class tx_solrdisablesitehash_DisableSitehashQueryModifier implements tx_solr_QueryModifier {
+
+	/**
+	 * Modifies the given query, currently removes the filter on the siteHash field
+	 *
+	 * @param	tx_solr_Query	The query to modify
+	 * @return	tx_solr_Query	The modified query without the siteHash filter
+	 */
+	public function modifyQuery(tx_solr_Query $query) {
+
+		if ($GLOBALS['TSFE']->tmpl->setup['plugin.']['tx_solr.']['query.']['disableSiteHash']) {
+			$query->removeFilter('siteHash');
+		}
+
+		return $query;
+	}
+}
+
+
+if (defined('TYPO3_MODE') && $TYPO3_CONF_VARS[TYPO3_MODE]['XCLASS']['ext/solr_disablesitehash/classes/class.tx_solrdisablesitehash_disablesitehashquerymodifier.php'])	{
+	include_once($TYPO3_CONF_VARS[TYPO3_MODE]['XCLASS']['ext/solr_disablesitehash/classes/class.tx_solrdisablesitehash_disablesitehashquerymodifier.php']);
+}
+
+?>
---
+++ ext_localconf.php
@@ -0,0 +1,10 @@
+<?php
+
+if (!defined ('TYPO3_MODE')) {
+	die ('Access denied.');
+}
+
+$TYPO3_CONF_VARS['EXTCONF']['solr']['modifySearchQuery']['removeSiteHashFilter'] = 'EXT:solr_disablesitehash/classes/class.tx_solrdisablesitehash_disablesitehashquerymodifier.php:tx_solrdisablesitehash_DisableSitehashQueryModifier';
+
+
+?>
