6e33453cdf7aada6a3cd91f4cc8b347505666d13
julian layen <layenjulian@gmail.com>
2015-06-08 16:32:00 +0200

    add ext_emconf file

---
+++ ext_emconf.php
@@ -0,0 +1,41 @@
+<?php
+
+/***************************************************************
+ * Extension Manager/Repository config file for ext "solr".
+ *
+ * Auto generated 08-06-2015 13:10
+ *
+ * Manual updates:
+ * Only the data in the array - everything else is removed by next
+ * writing. "version" and "dependencies" must not be touched!
+ ***************************************************************/
+
+$EM_CONF[$_EXTKEY] = array (
+	'title' => 'Solr Disablesitehash',
+	'description' => 'Solr Disablesitehash (please add plugin.tx_solr.query.disableSiteHash = 1 )',
+	'category' => 'plugin',
+	'version' => '3.0.0',
+	'state' => 'stable',
+	'uploadfolder' => true,
+	'createDirs' => '',
+	'clearcacheonload' => true,
+	'author' => 'Renan Gautier & Julian Layen',
+	'author_email' => 'renan.gautier@exl-group.com, julian.layen@exl-group.com',
+	'author_company' => 'Exl group',
+	'constraints' => 
+	array (
+		'depends' => 
+		array (
+			'php' => '5.3.0-0.0.0',
+			'typo3' => '4.5.0-6.2.99',
+			'solr' => '3.0.0-3.9.99',
+		),
+		'conflicts' => 
+		array (
+		),
+		'suggests' => 
+		array (
+		),
+	),
+);
+
