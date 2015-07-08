06d4e4ddf7150c12e54001544a64e7658b267d2d
Ingo Renner <ingo.renner@gmail.com>
2015-07-07 21:11:22 -0700

    Restore original emconf
    Updated values:
    * author
    * dependencies
    * version increased from original 1.0.0 to 1.1.0 (removed 3.0.0)

--- ext_emconf.php
+++ ext_emconf.php
@@ -10,32 +10,32 @@
  * writing. "version" and "dependencies" must not be touched!
  ***************************************************************/
 
-$EM_CONF[$_EXTKEY] = array (
-	'title' => 'Solr Disablesitehash',
-	'description' => 'Solr Disablesitehash (please add plugin.tx_solr.query.disableSiteHash = 1 )',
-	'category' => 'plugin',
-	'version' => '3.0.0',
-	'state' => 'stable',
-	'uploadfolder' => true,
-	'createDirs' => '',
-	'clearcacheonload' => true,
+$EM_CONF[$_EXTKEY] = array(
+	'title' => 'Solr - Sitehash Disabler',
+	'description' => 'Disables the use of the site hash filter in Solr queries.',
+	'version' => '1.1.0',
 	'author' => 'Solr Community Team',
 	'author_email' => 'solr-team@dkd.de',
 	'author_company' => 'Solr Community Team',
-	'constraints' => 
-	array (
-		'depends' => 
-		array (
-			'php' => '5.3.0-0.0.0',
-			'typo3' => '4.5.0-6.2.99',
-			'solr' => '3.0.0-3.9.99',
+	'category' => 'fe',
+	'conflicts' => '',
+	'priority' => '',
+	'module' => '',
+	'state' => 'stable',
+	'uploadfolder' => false,
+	'createDirs' => '',
+	'modify_tables' => '',
+	'clearCacheOnLoad' => true,
+	'lockType' => '',
+	'constraints' => array(
+			'depends' => array(
+				'php' => '5.3.0-0.0.0',
+				'typo3' => '4.5.0-6.2.99',
+				'solr' => '3.0.0-3.9.99',
+			),
+			'conflicts' => array(),
+			'suggests' => array(),
 		),
-		'conflicts' => 
-		array (
-		),
-		'suggests' => 
-		array (
-		),
-	),
+	'_md5_values_when_last_written' => 'a:4:{s:9:"ChangeLog";s:4:"d561";s:12:"ext_icon.gif";s:4:"11e4";s:17:"ext_localconf.php";s:4:"1a7a";s:69:"classes/class.tx_solrdisablesitehash_disablesitehashquerymodifier.php";s:4:"0e30";}',
 );
-
+?>
