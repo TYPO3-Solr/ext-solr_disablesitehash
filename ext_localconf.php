9375ad2ad50622ced714e4dcc6f60680b6d74879
Ingo Renner <ingo.renner@gmail.com>
2015-07-07 21:15:05 -0700

    Use uppercase Classes folder

--- ext_localconf.php
+++ ext_localconf.php
@@ -4,7 +4,7 @@ if (!defined ('TYPO3_MODE')) {
 	die ('Access denied.');
 }
 
-$TYPO3_CONF_VARS['EXTCONF']['solr']['modifySearchQuery']['removeSiteHashFilter'] = 'EXT:solr_disablesitehash/classes/class.tx_solrdisablesitehash_disablesitehashquerymodifier.php:tx_solrdisablesitehash_DisableSitehashQueryModifier';
+$TYPO3_CONF_VARS['EXTCONF']['solr']['modifySearchQuery']['removeSiteHashFilter'] = 'EXT:solr_disablesitehash/Classes/class.tx_solrdisablesitehash_disablesitehashquerymodifier.php:tx_solrdisablesitehash_DisableSitehashQueryModifier';
 
 
 ?>
